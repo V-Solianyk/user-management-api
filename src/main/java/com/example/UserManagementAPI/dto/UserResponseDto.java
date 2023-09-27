196eb38c2e1ad2178a04fe48cd8813db5d229512
V-Solianyk <112614843+V-Solianyk@users.noreply.github.com>
2023-09-27 17:29:47 +0300

    first commit(a)

---
+++ src/main/java/com/example/UserManagementAPI/dto/UserResponseDto.java
@@ -0,0 +1,13 @@
+package com.example.UserManagementAPI.dto;
+
+import java.time.LocalDate;
+
+public class UserResponseDto {
+    private Long id;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private LocalDate birthDate;
+    private String address;
+    private String phoneNumber;
+}
